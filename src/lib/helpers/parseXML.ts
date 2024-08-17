38ac3f39f91c7530502bac54136a9ba49e2fcefd
Kractero <kractero@sideroca.com>
2024-08-17 08:10:17 -0700

    use more ratelimiting headers

--- src/lib/helpers/parseXML.ts
+++ src/lib/helpers/parseXML.ts
@@ -7,31 +7,43 @@ export function sleep(ms: number) {
 }
 
 export async function parseXML(url: string, userAgent: string, password?: string) {
+
 	const headers: Record<string, string> = {
-		'User-Agent': `Used by ${userAgent} with Queries, written by Kractero`
+		'User-Agent': `Used by ${userAgent} with Hare, written by Kractero`
 	};
 
 	if (password) {
 		headers['X-Password'] = password;
 	}
 
-	const response = await fetch(`${url}&userAgent=Used by ${userAgent} with Queries, written by Kractero`, {
+	const response = await fetch(`${url}&userAgent=${userAgent} using Hare by Kractero`, {
 		method: 'GET',
 		headers
 	});
 
+    const ratelimitRemaining = Number(response.headers.get("RateLimit-Remaining"));
+    const ratelimitReset = Number(response.headers.get("RateLimit-Reset"));
+    const retryAfter = Number(response.headers.get('Retry-After'));
+
 	if (response.status === 404) {
-		return { "status": `failed with error code 404` }
+		return { status: `failed with error code 404` };
 	}
 
 	if (response.status === 409) {
-		return { "status": `failed with error code 409` }
+		return { status: `failed with error code 409` };
 	}
 
-	if (response.status === 429) {
-		await sleep(Number(response.headers.get('retry-after')) * 1000 + 2000)
-		return await parseXML(url, userAgent, password ? password : "")
-	}
+    if (response.status === 429) {
+        const waitTime = retryAfter > 0 ? retryAfter : (ratelimitReset / ratelimitRemaining);
+        await sleep(waitTime * 1000);
+        return await parseXML(url, userAgent, password ? password : '');
+    }
+
+    if (ratelimitRemaining > 0) {
+        await sleep((ratelimitReset / ratelimitRemaining) * 1000);
+    } else {
+        await sleep(ratelimitReset * 1000);
+    }
 
 	const xml = await response.text();
 	const xmlObj = parser.parse(xml);
