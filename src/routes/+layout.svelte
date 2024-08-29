1b0eddd51826f4a111e4f7cfbc4c2de6b7741601
Kractero <kractero@sideroca.com>
2024-08-29 10:06:13 -0700

    bazaar looking header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,32 @@
 <script lang="ts">
-	import { Computer, Github, Home, Moon, NotebookText, Sun } from 'lucide-svelte';
+	import { Computer, Home, Notebook } from 'lucide-svelte';
 	import '../app.css';
-	import { base } from "$app/paths"
+	import { ModeWatcher } from 'mode-watcher';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
 </script>
 
-<main class="flex min-h-screen flex-col items-center p-12 mb-16">
-	<div class="font-main mt-2 mb-2 text-center flex flex-col items-center">
-		<h1 class="text-7xl my-2 mb-8 tracking-tight">
-			Card <span class="text-purple-700">Queries</span>
-		</h1>
-		<div class="mb-8 flex">
-			<a href="{base}/" class="inline">
-				<Home />
-			</a>
-			<button
-				on:click={() => {
-					localStorage.setItem('theme', 'dark');
-					document.documentElement.classList.add('dark');
-				}}
+<ModeWatcher />
+<main class="flex min-h-screen flex-col items-center font-sans">
+	<header class="mb-10 flex flex-col items-center justify-center pt-40">
+		<h2 class="text-5xl font-bold">Card <span class="text-purple-700">Queries</span></h2>
+		<div class="mt-4 flex">
+			<Button variant="ghost" href="/"><Home class="size-6" /></Button>
+			<ThemeSwitcher />
+			<Button variant="ghost" href="/docs"><Notebook class="size-6" /></Button>
+			<Button variant="ghost" href="/dev"><Computer class="size-6" /></Button>
+			<Button variant="ghost" href="https://github.com/Kractero/kee">
+				<svg
+					class="size-6 dark:invert my-auto"
+					role="img"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					><title>GitHub</title><path
+						d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+					></path></svg
+				></Button
 			>
-				<Sun class="inline dark:hidden mx-2" />
-			</button>
-			<button
-				on:click={() => {
-					localStorage.setItem('theme', 'light');
-					document.documentElement.classList.remove('dark');
-				}}
-			>
-				<Moon class="hidden dark:inline mx-2" />
-			</button>
-			<a href="{base}/docs" id="docs" class="inline">
-				<NotebookText />
-			</a>
-			<a href="{base}/dev" id="dev" class="inline ml-2">
-				<Computer />
-			</a>
-			<a target="_blank" rel="noreferrer noopener" href="https://github.com/Kractero/kee" id="dev" class="inline ml-2">
-				<Github />
-			</a>
 		</div>
-	</div>
+	</header>
 	<slot />
 </main>
